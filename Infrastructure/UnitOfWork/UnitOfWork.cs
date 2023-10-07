5aa517762b81ad014f9f81b5eb7b478abcd4590b
Paola2005 <137318847+Paola2005@users.noreply.github.com>
2023-10-06 22:14:50 -0500

    feat: :sparkles:

--- Infrastructure/UnitOfWork/UnitOfWork.cs
+++ Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -8,25 +8,30 @@ using Infrastructure.Repositories;
 
 namespace Infrastructure.UnitOfWork;
 
-    public class UnitOfWork : IUnitOfWork, IDisposable
+public class UnitOfWork : IUnitOfWork, IDisposable
+{
+    private readonly VeterinariaContext _context;
+    private PaisRepository _paises;
+
+    public UnitOfWork(VeterinariaContext context)
     {
-        private readonly VeterinariaContext _context;
-        private readonly PaisRepository _paises;
-        public UnitOfWork(VeterinariaContext context)
-        {
-            _context = context;
-        }
-        
-
-
-        public async Task<int> SaveAsync()
-        {
-            return await _context.SaveChangesAsync();
-        }
-
-        public void Dispose()
-        {
-            _context.Dispose();
-        }
-        
+        _context = context;
     }
+    public IPais Paises{
+        get{
+            if (_paises==null){
+                _paises=new PaisRepository(_context);
+            }
+            return _paises;
+        }
+    }
+    public async Task<int> SaveAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
